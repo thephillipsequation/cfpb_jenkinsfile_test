677a8d93b477e3894c0935a46367d893fd9919aa
Jacob Castello <jacob.castello@cfpb.gov>
2017-10-25 20:04:50 -0400

    fods

--- this.py
+++ this.py
@@ -1,8 +1,6 @@
 print('This is the first line')
 print('This is the first line')
 print('This is the first line')
-print('Here\'s another line')
-print('Here\'s another line')
 print('This is the first line')
 print('Here\'s another line')
 print('Here\'s another line')
